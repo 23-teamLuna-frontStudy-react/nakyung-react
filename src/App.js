2e9df5fcaf25c26cf049f6e5e8453bcc62ee7f9e
Ahnnakyung <nakyung.ahn.03@gmail.com>
2023-09-07 02:06:27 +0900

    react-masterclass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Btn = styled.button`
+  color:white;
+  background-color: tomato;
+  border:0;
+  border-radius: 15px;
+`;
+
+const Link = styled(Btn)``
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Father as="header">
+    <Btn>Log in</Btn>
+    <Btn as="a" href="/">Log in</Btn>
+  </Father>
   );
 }
 

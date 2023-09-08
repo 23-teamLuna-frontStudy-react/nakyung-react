fe373bb8ce0450aebe4d64e912a4d40f3af966de
Ahnnakyung <nakyung.ahn.03@gmail.com>
2023-09-09 00:50:27 +0900

    styled-components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 import styled, {keyframes} from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100%{
-    transform:rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation:${rotationAnimation} 1s linear infinite;
+  background-color:${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
   <Wrapper>
-    <Box>
-      <span>🩷</span>
-    </Box>
+    <Title>Hello</Title>
   </Wrapper>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import App from './App';
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from 'styled-components';
+
+const darkTheme = {
+  textColor:"whitesmoke",
+  backgroundColor:"#111"
+};
+
+const lightTheme = {
+  textColor:"#111",
+  backgroundColor:"whitesmoke"
+};
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );

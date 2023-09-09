97663530e735448ed3e480c49d498acb5dc53610
Ahnnakyung <nakyung.ahn.03@gmail.com>
2023-09-09 20:25:29 +0900

    props

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="black" bgColor="#feecf7" />
+      <Circle bgColor="#191819" />
+    </div>
+  );
+}
+
+export default App;

aac79612a6201fb7ce19f8c08155b644477ae34a
Ahnnakyung <nakyung.ahn.03@gmail.com>
2023-09-10 21:25:19 +0900

    REACT ROUTER V6

---
+++ src/db.ts
@@ -0,0 +1,10 @@
+export const users = [
+    {
+        id: 1,
+        name: "nakyung",
+    },
+    {
+        id: 2,
+        name: "ahn",
+    },
+];
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import App from './App';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
-import { darkTheme, lightTheme } from './theme';
+import { RouterProvider } from 'react-router-dom';
+import router from './screens/Router';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <App />
-    </ThemeProvider>
+      <RouterProvider router={router}/>
   </React.StrictMode>
 );

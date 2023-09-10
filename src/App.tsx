b0d814909ad35f1f6cbf5cce17844ba0c78d2187
Ahnnakyung <nakyung.ahn.03@gmail.com>
2023-09-10 11:59:41 +0900

    typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+color:${(props) => props.theme.textColor};
+`;
 
 function App() {
   return (
-    <div>
-      <Circle borderColor="black" bgColor="#feecf7" />
-      <Circle bgColor="#191819" />
-    </div>
+    <Container>
+      <H1>proptected</H1>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={lightTheme}>
       <App />
+    </ThemeProvider>
   </React.StrictMode>
 );

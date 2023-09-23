4cfc52009eb1eba10e7e626b8d01b8079cd3ffc3
Ahnnakyung <nakyung.ahn.03@gmail.com>
2023-09-23 19:38:29 +0900

    ANIMATIONS

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,3 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "#ffeffa",
-  boardColor: "#DADFE9",
-  cardColor: "white",
-};
+export const darkTheme: DefaultTheme = {};
